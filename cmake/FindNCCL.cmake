a77670615daa3a5060090301f475f3c8bb51dd10
Zhang Jiejing <kzjeef@gmail.com>
2025-07-21 00:00:20 +0800

    cmake: nccl find lib without version.

--- cmake/FindNCCL.cmake
+++ cmake/FindNCCL.cmake
@@ -18,9 +18,26 @@ else()
 endif()
 
 message("find nccl with ${NCCL_LIBNAME}")
+# 优先查找带版本号的 nccl 库
 find_library(
-  AS_NCCL_LIBRARY ${NCCL_LIBNAME}
-  PATH_SUFFIXES lib lib64 nccl-${NCCL_VERSION}-cuda-${CUDA_VERSION}/lib64)
+  AS_NCCL_LIBRARY_VERSIONED
+  NAMES nccl-${NCCL_VERSION}
+  PATH_SUFFIXES lib lib64 nccl-${NCCL_VERSION}-cuda-${CUDA_VERSION}/lib64
+)
+
+# 如果没找到，再查找不带版本号的 nccl 库
+if(NOT AS_NCCL_LIBRARY_VERSIONED)
+   message("find nccl without version number, searching ${CUDAToolkit_LIBRARY_DIR}")
+  find_library(
+    AS_NCCL_LIBRARY
+    NAMES nccl
+    PATHS 
+    ${CUDAToolkit_LIBRARY_DIR}
+  )
+else()
+  message("found nccl with version number")
+  set(AS_NCCL_LIBRARY ${AS_NCCL_LIBRARY_VERSIONED})
+endif()
 
 if(ENABLE_NV_STATIC_LIB)
   message("add nccl static lib")
@@ -42,7 +59,7 @@ install(FILES ${NCCL_LIBS}
         DESTINATION ${CMAKE_INSTALL_LIBDIR})
 endif()
 
-
+message("find nccl at ${NCCL_INCLUDE_DIR} lib: ${AS_NCCL_LIBRARY}")
 find_package_handle_standard_args(NCCL DEFAULT_MSG NCCL_INCLUDE_DIR
                                   AS_NCCL_LIBRARY)
 
